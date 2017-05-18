28a4c68d49ae02a92befeed9ef7e85c64fd9ce9e
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-17 23:02:22 -0700

    Finished Footer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import Profile from './components/Profile';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Education from './components/Education';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <Experience />
         <Education />
         <hr />
+        <Footer />
       </div>
     )
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className='Footer'>
+        <p>28258 Peachtree Drive, Hayward, CA 94545</p>
+        <p><em>Available for Relocation</em></p>
+      </div>
+    )
+  }
+}
+
+export default Footer;
