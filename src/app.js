62e0fcb7c4847a4a4dfc1d9e75ba61dfb75f09f1
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-17 20:02:38 -0700

    Finished profile

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NamePlate from './components/NamePlate';
+import Profile from './components/Profile';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <NamePlate />
-        <hr />
+        <hr className='TopHr'/>
+        <Profile />
       </div>
     )
   }
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className='Profile'>
+       <h4 className='headline'><strong>PROFESSIONAL PROFILE:</strong></h4>
+       <p>Self-taught and highly motivated web developer with a wide range of technical
+          skills and a passion for writing quality code and learning new technologies.
+          Known to be highly adaptable, and thrives in environments that offer challenge
+          and opportunity to growth. An excellent communicator and excels as both a member
+          of a team, as well as an individual.
+       </p>
+      </div>
+    )
+  }
+}
+
+export default Profile;
