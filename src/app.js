dc339fc062be2220d556529f8212833f09417cf1
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-17 18:59:40 -0700

    Setup index, App, and NamePlate

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App Extends Component {
+  render() {
+    return (
+      <div className='App'>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/NamePlate.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NamePlate extends Component {
+  render() {
+    return (
+      <div className='NamePlate'>
+      </div>
+    )
+  }
+}
+
+export default NamePlate;
